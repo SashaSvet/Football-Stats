033852ff312b9103e9de2ebae4b48b1f12d2fcdb
Sasha Svet <alexander.svet8@gmail.com>
2020-09-03 19:33:53 +0300

    second commit

---
+++ WebDriver.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd 
+
+
+class Driver:
+    def __init__(self):
+        self.name = 'Driver'
+
+    # def getConnection(self):
---
+++ uscrapper.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+
+
+class Uscrapper:
+    def __init__(self):
+        self.path = 'www.understat.com'
+
+
+    
